152f2e096ab1185310c1422b8aa961155f705a00
weihuawei <weihuawei@corp.netease.com>
2022-09-27 18:19:22 +0800

    my net frame 0.2.2 优化代码

--- server_process/hotfix/myreload.py
+++ server_process/hotfix/myreload.py
@@ -14,7 +14,7 @@ def Init():
 	timer.Call_out(5, "ProjReload", MyReload)
 
 def MyReload():
-	PrintDebug("reloading ... ")
+	PrintNotify("reloading ... ")
 	LookFile(True, True)
 	timer.Call_out(5, "ProjReload", MyReload)
 
@@ -27,8 +27,7 @@ def LookFile(bReload = False, bNotifyNew = False):
 		ReloadPyFile(sCurPath, sName, bReload, bNotifyNew)
 
 def ReloadPyFile(sCurPath, sName, bReload, bNotifyNew):
-	import os
-	import importlib
+	import sys
 	global FILE_LIST
 	if sName.endswith(".py"):
 		bNew = False
@@ -36,17 +35,18 @@ def ReloadPyFile(sCurPath, sName, bReload, bNotifyNew):
 			bNew = True
 			FILE_LIST.append(sName)
 		if bReload:
-			from importlib import reload
+			from importlib import reload, import_module
 			sMod = sName.split(".")[0]
-			obj = importlib.import_module(sMod)
+			obj = import_module(sMod)
 			if bNotifyNew and bNew:
 				PrintNotify("new file is reloading %s ..."%(sMod))
 			reload(obj)
+			sys.modules[sMod] = obj
 			func = getattr(obj, "OnReload", None)
 			if func:
 				func()
 	elif "." not in sName:
-		import sys
+		import os
 		sCurPath = sCurPath + "\%s"%sName
 		sys.path.append(sCurPath)
 		lstFile = os.listdir(sCurPath)
