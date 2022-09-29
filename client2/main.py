da5bd1d7900224b921f8ee2c0938067b15eda6a6
weihuawei <weihuawei@corp.netease.com>
2022-09-29 16:51:35 +0800

    my net frame 0.2.3 优化client2代码

--- client2/main.py
+++ client2/main.py
@@ -12,8 +12,7 @@ if __name__ == "__main__":
 
 	oSendMq = mq.GetMq(MSGQUEUE_SEND)
 	oRecvMq = mq.GetMq(MSGQUEUE_RECV)
-	oProcessUI = multiprocessing.Process(target = script.main, args=(oSendMq, oRecvMq, ))
 	oProcessNet = multiprocessing.Process(target = net.main, args=(oSendMq, oRecvMq, ))
 
-	oProcessUI.start()
 	oProcessNet.start()
+	script.main(oSendMq, oRecvMq)
